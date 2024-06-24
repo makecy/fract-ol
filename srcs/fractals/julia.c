bd1bd9f43a240ca6d9b2de15d62f19fcc4c51f46
makecy <martinstefanoski9@hotmail.com>
2024-06-25 01:39:46 +0200

    need to debugg julia

--- srcs/fractals/julia.c
+++ srcs/fractals/julia.c
@@ -29,3 +29,36 @@ int	julia(t_fract *f, double zr, double zi)
     }
     return (n);
 }
+
+void render_julia(t_fract *f)
+{
+    int x = 0;
+	int y = 0;
+	int i = 0;
+    double zr, zi;
+    mlx_image_t *img;
+    int color = 0;
+
+    img = mlx_new_image(f->lib_mlx_ptr, WIDTH, HEIGHT);
+	while (x < WIDTH)
+    {
+		y = 0;
+		while (y < HEIGHT)
+        {
+            zr = f->min_r + (double)x * (f->max_r - f->min_r) / WIDTH;
+            zi = f->max_i + (double)y * (f->min_i - f->max_i) / HEIGHT;
+            i = julia(f, zr, zi);
+            if (i == MAX_ITERATIONS)
+                mlx_put_pixel(img, x, y, 0);
+            else
+            {
+                color = get_color(i);
+                mlx_put_pixel(img, x, y, color);
+            }
+			y++;
+        }
+		x++;
+    }
+    mlx_image_to_window(f->lib_mlx_ptr, img, 0, 0);
+    mlx_delete_image(f->lib_mlx_ptr, img);
+}
